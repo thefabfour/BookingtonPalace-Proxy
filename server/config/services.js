768fde17f9a8c77b71db7c728fe39ea54e2b1023
Anthony Patterson <pattersonanthony14@gmail.com>
2021-01-27 19:06:11 -0500

    integrates all three services. Need to adjust params to make the css styling carry over

--- server/config/services.js
+++ server/config/services.js
@@ -11,7 +11,7 @@ module.exports = {
     bundle: 'bundle.js',
   },
   service3: {
-    api: '/api/home/:id/photos',
+    api: '/home/:id/photos',
     url: 'http://localhost:3003',
     bundle: 'bundle.js',
   },
